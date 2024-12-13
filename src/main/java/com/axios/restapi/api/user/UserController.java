16f885d788ea6f435ce2e295168e6d74dc7b5836
jh_lee <juhyean8723@gmail.com>
2024-12-13 17:28:04 +0900

    mapper 패키지 api 계층으로 이동

--- src/main/java/com/axios/restapi/api/user/UserController.java
+++ src/main/java/com/axios/restapi/api/user/UserController.java
@@ -1,9 +1,12 @@
 package com.axios.restapi.api.user;
 
+import com.axios.restapi.api.user.response.ListUserResponse;
 import com.axios.restapi.business.user.UserService;
 import com.axios.restapi.api.user.request.CreateUserRequest;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,11 @@ import java.net.URI;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<ListUserResponse> readManyUsers() {
+        return ResponseEntity.ok(null);
+    }
 
     @PostMapping
     public ResponseEntity<Void> createUser(CreateUserRequest request) {
